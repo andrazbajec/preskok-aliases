af4bc1df4cdca17fa0652610098795f3515a9197
Deepy <andraz.bajec27@gmail.com>
2022-05-19 08:23:28 +0200

    Added b2b react commands

--- projects/b2b.sh
+++ projects/b2b.sh
@@ -10,11 +10,8 @@ alias b2b-backend-push="b2b-backend && gpfl"
 alias b2b-core-push="b2b-core && gpfl"
 alias b2b-frontend-push="b2b-frontend && gpfl"
 
-function b2b-pull-all {
-	foreach x ( b2b b2b-api b2b-backend b2b-core b2b-frontend )
-		gcm
-		ggpur
-	end
-}
+alias b2b-dev="b2b && npm run dev"
+alias b2b-start="b2b && npm run start"
+alias b2b-storybook="b2b && npm run storybook"
 
 alias b2b-docker="docker exec -it devenv2_b2b-carmarket_1 /bin/bash"
